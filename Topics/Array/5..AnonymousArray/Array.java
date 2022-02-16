93956514efe9db486e5f3d9a592acc6685a00cf0
Priyansusahoo <priyansusahoo1@gmail.com>
2022-02-16 21:12:53 +0530

    new .xml, .class files added

--- Topics/Array/5..AnonymousArray/Array.java
+++ Topics/Array/5..AnonymousArray/Array.java
@@ -1,5 +1,5 @@
 class Array {
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String[] args){
+        System.out.println("Hello");
     }
 }
