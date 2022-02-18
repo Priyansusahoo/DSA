723b6af592b09fcd7ab72f2be911d17226788846
Priyansusahoo <priyanusahoo1@gmail.com>
2022-02-18 21:19:18 +0530

    added project 5(anonymous array) & 6(print array elements)

--- Topics/Array/5..AnonymousArray/Array.java
+++ Topics/Array/5..AnonymousArray/Array.java
@@ -1,5 +1,16 @@
 class Array {
-    public static void main(String[] args){
-        System.out.println("Hello");
+    public static void main(String[] args) {
+        /**
+         * new anonymous array
+         */
+        Array.sum(new int[] { 10, 20, 30 });
+    }
+
+    static void sum(int[] no) {
+        int total = 0;
+        for (int i : no) {
+            total = total + i;
+        }
+        System.out.println(total);
     }
 }
---
+++ Topics/Array/6/ArrayPrint/src/com/priyansu/Main.java
@@ -0,0 +1,13 @@
+package com.priyansu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        /**
+         * print array elements in 4 diffrent ways
+         */
+
+
+    }
+}
