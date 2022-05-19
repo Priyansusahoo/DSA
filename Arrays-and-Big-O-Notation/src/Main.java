1ae2862ff5edb549ba4747a720449ec7594ed7c4
Priyansusahoo <priyansusahoo1@gmail.com>
2022-05-19 14:33:56 +0530

    Added: Steps/procedureof the Code

--- Arrays-and-Big-O-Notation/src/Main.java
+++ Arrays-and-Big-O-Notation/src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        /**
+         * Create an array containing 7 elements.
+         * Declaring an integer value, which we want to find in the array.
+         * Then, the loop & break statement to find the index of the element
+         */
         int[] intArray = new int[7];
-
+        //Array
         intArray[0] = 20;
         intArray[1] = 35;
         intArray[2] = -15;
@@ -10,8 +14,9 @@ public class Main {
         intArray[4] = 55;
         intArray[5] = 1;
         intArray[6] = -22;
-
+        //element we want to find
         int index = -1;
+        //statement to find the index
         for (int i = 0;i < intArray.length;i++) {
 //            System.out.println(intArray[i]);
             if (intArray[i] == 7) {
@@ -20,6 +25,5 @@ public class Main {
             }
         }
         System.out.println("index = " + index);
-
     }
 }
